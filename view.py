f3a7df12b572dc9c35bc90a08585325046af8842
Funyn <461547449@qq.com>
2017-11-26 21:18:18 +0800

    完成测试功能

--- view.py
+++ view.py
@@ -5,5 +5,9 @@ from django.shortcuts import render,HttpResponse
 
 	        def africa(request):
 		        return HttpResponse('非洲专区')
+<<<<<<< Updated upstream
 def live():
     print('加入直播的功能的bug的修复')
+=======
+#开发测试完成
+>>>>>>> Stashed changes
