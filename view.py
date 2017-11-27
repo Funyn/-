b4bd0616a236a30e2de12faa18edfcd917318f34
Funyn <461547449@qq.com>
2017-11-27 09:54:31 +0800

    紧急BUG修复

--- view.py
+++ view.py
@@ -7,7 +7,7 @@ from django.shortcuts import render,HttpResponse
 		        return HttpResponse('非洲xxxxxxx专区')
 
 def live():
-    print('加入直播的功能的bugxxxxxx的修复')
+    print('加入直播的功能的bug的修复')
 
 #开发测试进行中
 
